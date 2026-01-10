76c2854a03ac8e68b8ebde35c0eacdcd3a6b9e14
Tefi <s.mercado01@ufromail.cl>
2026-01-10 18:13:31 -0300

    Cambio a adaptador Vercel y correcciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import node from '@astrojs/node';
-
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
-  adapter: node({
-    mode: 'standalone'
-  })
+  output: 'server', 
+  adapter: vercel(),
 });
