967229a94bb3eb83e99ebde51d70d75870ef461f
Tefi <s.mercado01@ufromail.cl>
2026-01-28 15:24:09 -0300

    Cambio adaptador node a vercerl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel/serverless'; // Cambiamos node por vercel
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone', // <--- ESTO ES CRUCIAL
-  }),
+  adapter: vercel(), // Usamos el adaptador de vercel
   integrations: [react()],
 });
